09d0aa9bb8912ffe2d87cf313f210205a9a1f2bd
Kyle Carter <kcarter@ctatechs.com>
2013-01-11 12:20:10 -0600

    Fixed class checking; updated Gemfile

--- lib/tablinate.rb
+++ lib/tablinate.rb
@@ -8,7 +8,7 @@ module Tablinate
   def self.generate_table(object, params={})
     #turns an ActiveRecord::Relation into an array of hashes.
     object = JSON.parse(object) if object.class == String
-    object = object.collect{ |x| x.attributes } unless object[0].class == Array
+    object = object.collect{ |x| x.attributes } unless object.class == Array
     table = HTML::Tag.generate_tag("table", params)
     #thead
     table += HTML::Tag.table_head(object[0].keys, params)
